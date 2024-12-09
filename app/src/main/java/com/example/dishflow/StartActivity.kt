7a3d7ea5366604562ca789797d5fe5882c7cb8ef
Harsh Galiyawala <109781796+harshGALIYAWALA@users.noreply.github.com>
2024-12-09 17:39:33 +0530

    first change

---
+++ app/src/main/java/com/example/dishflow/StartActivity.kt
@@ -0,0 +1,33 @@
+package com.example.dishflow
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class StartActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_start)
+
+        val next_btn = findViewById<Button>(R.id.next_btn)
+
+        next_btn.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
