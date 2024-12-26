b0211800e3d42e3883909665af18e5601b84cf69
Harsh Galiyawala <109781796+harshGALIYAWALA@users.noreply.github.com>
2024-12-26 16:50:11 +0530

    USER UI is done !!!

--- app/src/main/java/com/example/dishflow/activities/DetailsActivity.kt
+++ app/src/main/java/com/example/dishflow/activities/DetailsActivity.kt
@@ -25,6 +25,12 @@ class DetailsActivity : AppCompatActivity() {
 
 
 
+        binding.imageButton.setOnClickListener{
+            finish()
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
