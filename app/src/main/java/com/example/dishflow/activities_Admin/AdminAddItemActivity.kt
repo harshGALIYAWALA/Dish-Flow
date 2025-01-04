3b3c1a2272d4c0fb2934694c4ef410a49f891e6e
Harsh Galiyawala <109781796+harshGALIYAWALA@users.noreply.github.com>
2025-01-04 18:05:13 +0530

    added funality

--- app/src/main/java/com/example/dishflow/activities_Admin/AdminAddItemActivity.kt
+++ app/src/main/java/com/example/dishflow/activities_Admin/AdminAddItemActivity.kt
@@ -2,20 +2,44 @@ package com.example.dishflow.activities_Admin
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.dishflow.R
+import com.example.dishflow.databinding.ActivityAdminAddItemBinding
 
 class AdminAddItemActivity : AppCompatActivity() {
+    private val binding: ActivityAdminAddItemBinding by lazy {
+        ActivityAdminAddItemBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_admin_add_item)
+        setContentView(binding.root)
+
+        binding.selectImage.setOnClickListener{
+            pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    // image selector from photo
+    val pickImage =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if(uri != null) {
+                binding.selectedImage.setImageURI(uri)
+            }
+
+        }
+
+
 }
