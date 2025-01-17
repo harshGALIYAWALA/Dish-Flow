559bb47fc9d2f148b490ff12aaccaad49d304a79
Harsh Galiyawala <109781796+harshGALIYAWALA@users.noreply.github.com>
2025-01-17 12:35:28 +0530

    backend started!!!

--- app/src/main/java/com/example/dishflow/activities_Admin/AdminSignUpActivity.kt
+++ app/src/main/java/com/example/dishflow/activities_Admin/AdminSignUpActivity.kt
@@ -2,14 +2,34 @@ package com.example.dishflow.activities_Admin
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.dishflow.R
 import com.example.dishflow.databinding.ActivityAdminSignUpBinding
+import com.example.dishflow.models.UserModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
 
 class AdminSignUpActivity : AppCompatActivity() {
+
+    private lateinit var email: String
+    private lateinit var password: String
+    private lateinit var username: String
+    private lateinit var nameOfRestaurant: String
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
+
+
 
     private val binding : ActivityAdminSignUpBinding by lazy {
         ActivityAdminSignUpBinding.inflate(layoutInflater)
@@ -20,10 +40,68 @@ class AdminSignUpActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
+        //initialize auth and database
+        auth = Firebase.auth
+        database = Firebase.database.reference
+
+        // creating user
+        binding.createUserButton.setOnClickListener{
+
+            username = binding.name.text.toString().trim()
+            nameOfRestaurant = binding.restaurantName.text.toString().trim()
+            email = binding.email.text.toString().trim()
+            password = binding.password.text.toString().trim()
+
+            // Validation
+            if (username.isBlank() || nameOfRestaurant.isBlank() || email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "please fill all detail", Toast.LENGTH_SHORT).show()
+            } else{
+                createUser(email, password)
+            }
+
+        }
+
+        //redirect to login screen
         binding.adminRidirectToLogin.setOnClickListener{
             intent = Intent(this, AdminLogInActivity::class.java)
             startActivity(intent)
         }
+
+
+        // option for region
+        val locationList = arrayOf(
+            "Andhra Pradesh",
+            "Arunachal Pradesh",
+            "Assam",
+            "Bihar",
+            "Chhattisgarh",
+            "Goa",
+            "Gujarat",
+            "Haryana",
+            "Himachal Pradesh",
+            "Jharkhand",
+            "Karnataka",
+            "Kerala",
+            "Madhya Pradesh",
+            "Maharashtra",
+            "Manipur",
+            "Meghalaya",
+            "Mizoram",
+            "Nagaland",
+            "Odisha",
+            "Punjab",
+            "Rajasthan",
+            "Sikkim",
+            "Tamil Nadu",
+            "Telangana",
+            "Tripura",
+            "Uttar Pradesh",
+            "Uttarakhand",
+            "West Bengal"
+        )
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
+        val autoCompleteTextView = binding.listOfLocation
+        autoCompleteTextView.setAdapter(adapter)
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -31,5 +109,31 @@ class AdminSignUpActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun createUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{ task ->
+            if(task.isSuccessful){
+                Toast.makeText(this, "account is created", Toast.LENGTH_SHORT).show()
+                saveUserData()
+                intent = Intent(this, AdminLogInActivity::class.java)
+                startActivity(intent)
+            } else{
+                Toast.makeText(this, "account is not created", Toast.LENGTH_SHORT).show()
+                Log.d("Account", "createUser: Failure", task.exception)
+            }
+        }
+    }
+
+    private fun saveUserData() {
+        username = binding.name.text.toString().trim()
+        nameOfRestaurant = binding.restaurantName.text.toString().trim()
+        email = binding.email.text.toString().trim()
+        password = binding.password.text.toString().trim()
+
+        val user  = UserModel(username, nameOfRestaurant, email, password)
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+        database.child("user").child(userId).setValue(user)
+
     }
 }
